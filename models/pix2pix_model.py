c16ee7fc50ec9b5af3e76c04117acd24caf1a387
Dang Trong Bao Thi <dangtrong.baothi@gmail.com>
2026-05-24 10:59:35 +0700

    feat: add freeze_encoder argument

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -55,8 +55,9 @@ class Pix2PixModel(BaseModel):
             self.model_names = ["G"]
         self.device = opt.device
         # define networks (both generator and discriminator)
-        self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.norm, not opt.no_dropout, opt.init_type, opt.init_gain)
-
+        self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG,
+                    opt.norm, not opt.no_dropout, opt.freeze_encoder,
+                    opt.init_type, opt.init_gain, opt.device)
         if self.isTrain:  # define a discriminator; conditional GANs need to take both input and output images; Therefore, #channels for D is input_nc + output_nc
             self.netD = networks.define_D(opt.input_nc + opt.output_nc, opt.ndf, opt.netD, opt.n_layers_D, opt.norm, opt.init_type, opt.init_gain)
 
